ce66d55786496cea084327183217f1cb83517187
honeydewkay <hyeonjukim13@naver.com>
2023-03-05 21:03:57 +0900

    test

---
+++ ch2/src/CharToCode.java
@@ -0,0 +1,20 @@
+public class CharToCode {
+    public static void main(String[] args) {
+        /*
+        char ch = 'A';
+        int code = (int)ch;
+
+        System.out.printf("%c=%d%n : (%#X)", ch, code, code);
+
+        char hch = '가';
+        System.out.printf("%c=%d%n : (%#X) ", hch, (int)hch, (int)hch);
+        */
+
+        System.out.println('\'');
+        System.out.println("abc\t123\b456");
+        System.out.println('\n');
+        System.out.println("\"Hello\"");
+        System.out.println("c:\\");
+
+    }
+}
