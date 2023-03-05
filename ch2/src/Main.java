3580afea426b57bd898b0881f5162de634eb4bd8
honeydewkay <hyeonjukim13@naver.com>
2023-03-05 18:13:45 +0900

    Initial commit

---
+++ ch2/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        EX2 Kim = new EX2(100, "김현주"); //클래스 = EX2 ,
+
+        Kim.koreaSubject("국어", 100, 5);
+        Kim.setMathSubject("수학", 80, 10);
+            //3333
+        Kim.showscore();
+        ///sadasdsa
+
+        Kim.showscore();
+
+    }
+}
